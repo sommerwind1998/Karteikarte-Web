ef1ded88b22ddefee2f849c298d9100389392c47
sommerwind1998 <71171294+sommerwind1998@users.noreply.github.com>
2020-09-15 12:43:29 +0200

    Add database Creation File

---
+++ karteikarten_web.sql
@@ -0,0 +1,104 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE DATABASE IF NOT EXISTS `karteikarten_web` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `karteikarten_web`;
+
+CREATE TABLE IF NOT EXISTS `antwort` (
+  `antwort_id` int(11) NOT NULL AUTO_INCREMENT,
+  `antwort_text` varchar(255) NOT NULL,
+  `antwort_bild` int(11) NOT NULL,
+  PRIMARY KEY (`antwort_id`),
+  KEY `antwort_bild` (`antwort_bild`)
+) ENGINE=InnoDB AUTO_INCREMENT=47 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `benutzer` (
+  `benutzer_id` int(11) NOT NULL AUTO_INCREMENT,
+  `benutzer_name` varchar(255) NOT NULL,
+  `passwort_hash` varchar(255) NOT NULL,
+  `klasse` int(11) NOT NULL,
+  PRIMARY KEY (`benutzer_id`),
+  KEY `klasse` (`klasse`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `bild` (
+  `bild_id` int(11) NOT NULL AUTO_INCREMENT,
+  `bild_daten` text NOT NULL,
+  PRIMARY KEY (`bild_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `fach` (
+  `fach_id` int(11) NOT NULL AUTO_INCREMENT,
+  `fach_name` varchar(255) NOT NULL,
+  `klasse` int(11) NOT NULL,
+  PRIMARY KEY (`fach_id`),
+  KEY `klasse` (`klasse`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `frage` (
+  `frage_id` int(11) NOT NULL AUTO_INCREMENT,
+  `frage_text` varchar(255) NOT NULL,
+  `frage_bild` int(11) NOT NULL,
+  PRIMARY KEY (`frage_id`),
+  KEY `frage_bild` (`frage_bild`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `karteikarte` (
+  `karteikarte_id` int(11) NOT NULL AUTO_INCREMENT,
+  `thema` int(11) NOT NULL,
+  `frage` int(11) NOT NULL,
+  `antwort` int(11) NOT NULL,
+  PRIMARY KEY (`karteikarte_id`),
+  KEY `thema` (`thema`),
+  KEY `frage` (`frage`),
+  KEY `antwort` (`antwort`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `klasse` (
+  `klasse_id` int(11) NOT NULL AUTO_INCREMENT,
+  `klasse_name` varchar(255) NOT NULL,
+  PRIMARY KEY (`klasse_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `thema` (
+  `thema_id` int(11) NOT NULL AUTO_INCREMENT,
+  `thema_name` varchar(255) NOT NULL,
+  `fach` int(11) NOT NULL,
+  `code` varchar(100) NOT NULL,
+  PRIMARY KEY (`thema_id`),
+  UNIQUE KEY `code` (`code`),
+  KEY `fach` (`fach`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4;
+
+
+ALTER TABLE `antwort`
+  ADD CONSTRAINT `antwort_ibfk_1` FOREIGN KEY (`antwort_bild`) REFERENCES `bild` (`bild_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `benutzer`
+  ADD CONSTRAINT `benutzer_ibfk_1` FOREIGN KEY (`klasse`) REFERENCES `klasse` (`klasse_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `fach`
+  ADD CONSTRAINT `fach_ibfk_1` FOREIGN KEY (`klasse`) REFERENCES `klasse` (`klasse_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `frage`
+  ADD CONSTRAINT `frage_ibfk_1` FOREIGN KEY (`frage_bild`) REFERENCES `bild` (`bild_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `karteikarte`
+  ADD CONSTRAINT `karteikarte_ibfk_1` FOREIGN KEY (`thema`) REFERENCES `thema` (`thema_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `karteikarte_ibfk_2` FOREIGN KEY (`frage`) REFERENCES `frage` (`frage_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `karteikarte_ibfk_3` FOREIGN KEY (`antwort`) REFERENCES `antwort` (`antwort_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+ALTER TABLE `thema`
+  ADD CONSTRAINT `thema_ibfk_1` FOREIGN KEY (`fach`) REFERENCES `fach` (`fach_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
