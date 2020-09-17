e653d263b6e6b5d0f317af7df8c11565fcfe0c81
sommerwind1998 <71171294+sommerwind1998@users.noreply.github.com>
2020-09-17 09:24:16 +0200

    Insert Fach

---
+++ insert_fach.php
@@ -0,0 +1,27 @@
+<?php
+
+//Fach in Datenbank speichern
+
+$fach = $_POST["fach"];
+session_start();
+$klasse = $_SESSION["klasse"];
+
+$params = include("datenbankparameter.php");
+$conn = new mysqli($params["host"], $params["username"], $params["password"], $params["database"]);
+
+$sql = 'INSERT INTO fach (fach_name, klasse) VALUES (?, ?)';
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('si', $fach, $klasse);
+$stmt->execute();
+
+$id = $conn->insert_id;
+
+$conn->close(); 
+
+echo ('
+	<html>
+		<head>
+			<meta http-equiv="refresh" content="0; url=faecher_uebersicht.php?highlight='.$id.'" />
+		</head>
+	</html>
+');
