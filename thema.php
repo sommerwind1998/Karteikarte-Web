42d39b79e6666473048f93dd7f818cb6966411e5
sommerwind1998 <71171294+sommerwind1998@users.noreply.github.com>
2020-09-17 08:51:23 +0200

    Update thema.php

--- thema.php
+++ thema.php
@@ -1,6 +1,6 @@
 <?php
 
-//Übersicht der Themen eines Faches
+//Übersicht über ein Thema
 
 include "html_helper.php";
 
@@ -12,6 +12,19 @@ $logged_in = isset($_SESSION["user"]);
 session_write_close();
 
 if ($logged_in){
+
+$user = $_SESSION["user"];
+
+$admin=false;
+$sql = 'SELECT admin FROM benutzer WHERE benutzer_name = ?';
+$stmt = $conn->prepare($sql);
+$stmt->bind_param('s', $user);
+$stmt->execute();
+$result = $stmt->get_result();
+if ($result->fetch_assoc()["admin"] == 1)
+{
+	$admin=true;
+}
 
 $sql = 'SELECT thema_name, code FROM thema WHERE thema_id = ?';
 $stmt = $conn->prepare($sql);
@@ -51,12 +64,20 @@ $output = '
     <tr>
       <th scope="col">Frage</th>
       <th scope="col">Antwort</th>
+	  ';
+	  
+	if ($admin)
+	{
+		$output .= '<th scope="col">Löschen</th>';
+	}
+	  
+	$output .=  '
     </tr>
   </thead>
   <tbody>
 ';
 
-$sql = 'SELECT f.frage_text, a.antwort_text FROM karteikarte k
+$sql = 'SELECT f.frage_text, a.antwort_text, k.karteikarte_id FROM karteikarte k
 		INNER JOIN frage f ON k.frage=f.frage_id
 		INNER JOIN antwort a ON k.antwort=a.antwort_id
 		WHERE k.thema = ?
@@ -70,11 +91,24 @@ while($row = $result->fetch_assoc())
 {
 	$frage = $row["frage_text"];
 	$antwort = $row["antwort_text"];
+	$id = $row["karteikarte_id"];
 	
 	$output .= '
     <tr>
-      <td>'.$frage.'</td>
-      <td>'.$antwort.'</td>
+      <td style="width:45%">'.$frage.'</td>
+      <td style="width:45%">'.$antwort.'</td>
+	';
+	
+	if ($admin)
+	{
+		$output .= '<th scope="col">
+		<a href="loesche_karteikarte.php?id='.$id.'">
+			<img src="delete.png" width="30" height="30" alt="Löschen">
+		</a>
+		</th>';
+	}
+	
+	$output .= '
     </tr>
 	';
 	
